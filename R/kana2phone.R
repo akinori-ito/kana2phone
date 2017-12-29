d51fa56236ddf0a1e4e3bf7dde5ebff46741ac23
Akinori Ito <aito@fw.ipsj.or.jp>
2017-12-29 09:33:46 +0900

    first commit

---
+++ R/kana2phone.R
@@ -0,0 +1,43 @@
+# かな表記を音素表記の配列に変換
+
+# 音素体系
+# 
+# silB silE
+# N a a: i i: u u: e e: o o:
+# w y
+# b by ch d f g gy h hy j k ky m my n ny p py q r ry s sh t ts z
+
+
+K2P_TBL <- read.csv("kanaphone.csv",as.is=T,fileEncoding="UTF-8")
+
+#' Convert a kana string to a phone vector
+#' @param str a katakana string
+#' @importfrom stringr str_length
+#' @importfrom stringr str_split
+#' @export
+#'
+kana2phone <- function(str) {
+    res <- c()
+    i <- 1
+    while (i <= str_length(str)) {
+      ch <- substr(str,i,i)
+      i <- i+1
+      if (i < str_length(str) && 
+	      substr(str,i,1) %in% c("ァ","ィ","ゥ","ェ","ォ","ャ","ュ","ョ")) {
+         ch <- substr(str,i-1,i)
+         i <- i+1
+      }
+      p <- match(ch,K2P_TBL$kana)
+      if (is.na(p)) {
+         stop(paste("Can't convert",ch,"into phonemes"))
+      }
+      p <- K2P_TBL$phone[p]
+      if (i < str_length(str) && substr(str,i,i) == "ー") {
+         p <- paste(p, ":", sep="")
+         i <- i+1
+      }
+      res <- c(res,p)
+    }
+    str_split(paste(res,collapse=" ")," ")
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# kana2phone
+kana2phone converts a katakana string (Japanese) into a vector of phones in Japanese. Here's the phone inventory:
+
+Vowels: N a a: i i: u u: e e: o o:
+Semivowels: w y
+Consonants: b by ch d f g gy h hy j k ky m my n ny p py q r ry s sh t ts z
