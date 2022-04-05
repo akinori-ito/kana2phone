6a2662da86c94ef1e341d5830c7ee82478c12037
Akinori Ito <aito@spcom.ecei.tohoku.ac.jp>
2022-04-06 08:42:20 +0900

    katanaka2hiragana() added

--- R/hiragana2katakana.R
+++ R/hiragana2katakana.R
@@ -18,3 +18,23 @@ hiragana2katakana <- function(x) {
     }
     paste(r,collapse="")
 }
+
+#'
+#' Convert katakana to hiragana
+#' @param x a string (contains katakana)
+#' @return a string where all katananas are substituted into hiraganas
+#' @importFrom stringr str_length
+#' @export
+katakana2hiragana <- function(x) {
+    r <- c()
+    for (i in 1:str_length(x)) {
+        ch <- substr(x,i,i)
+        k <- match(ch,H2K_TBL$Kata)
+        if (!is.na(k)) {
+            r <- c(r,H2K_TBL$Hira[k])
+        } else {
+            r <- c(r,ch)
+        }
+    }
+    paste(r,collapse="")
+}
