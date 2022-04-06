4eb116ae21d5d4692b4c3c97c90d15b41360bc5e
Akinori Ito <aito@spcom.ecei.tohoku.ac.jp>
2022-04-06 11:03:59 +0900

    roman2katanaka() added

---
+++ R/roman2katakana.R
@@ -0,0 +1,45 @@
+#'
+#' Convert romaji to katakana
+#' @param rom a string (romaji)
+#' @return a string where all romaji are substituted into katakanas
+#' @importFrom stringr str_length
+#' @export
+roman2katakana <- function(rom) {
+  state <- 1
+  out <- c()
+  for (i in 1:str_length(rom)) {
+    ch <- substr(rom,i,i)
+    r <- R2K_TBL[[state]]
+    if (ch %in% names(r)) {
+      rr <- r[[ch]]
+      if (str_length(rr$output) > 0) {
+        out <- c(out,rr$output)
+      }
+      state <- rr$nextstate
+    } else {
+      if (state == 10) {
+        # 'n' remains
+        out <- c(out,NN)
+      }
+      out <- c(out,ch)
+      state <- 1
+    }
+  }
+  if (state == 10) {
+    # 'n' remains
+    out <- c(out,NN)
+  }
+  paste(out,collapse="")
+}
+
+# x <- read.xlsx("../../state.xlsx")
+# x$output[is.na(x$output)] <- ""
+# R2K_TBL<-list()
+# for (st in 1:32) {
+#   R2K_TBL[[st]] <- list()
+# }
+# for (i in 1:nrow(x)) {
+#   st <- x$state[i]+1
+#   input <- x$input[i]
+#   R2K_TBL[[st]][[input]]<-list(output=x$output[i],nextstate=x$nextstate[i]+1)
+# }
