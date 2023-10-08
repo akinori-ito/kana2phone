56d34c51284540122ded40660dbfb16b71abe656
Akinori Ito <a_ito@nifty.com>
2023-10-08 16:51:52 +0900

    K2P_TBLの文字コードを修正。Windows, Linuxの文字コードに対応。

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function() {
+  if (.Platform$OS.type == "windows") {
+    K2P_TBL$kana <- iconv(K2P_TBL$kana,from="UTF-8",to="SHIFT_JIS")
+    H2K_TBL$Hira <- iconv(H2K_TBL$Hira,from="UTF-8",to="SHIFT_JIS")
+    H2K_TBL$Kata <- iconv(H2K_TBL$Kata,from="UTF-8",to="SHIFT_JIS")
+  }
+}
+
