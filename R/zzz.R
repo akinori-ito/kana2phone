7266e8b612f5c8ac4e4542684d61a7c0635847c6
Akinori Ito <a_ito@nifty.com>
2023-10-08 16:54:36 +0900

    .onLoad() bugfix

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function() {
+.onLoad <- function(libname,pkgname) {
   if (.Platform$OS.type == "windows") {
     K2P_TBL$kana <- iconv(K2P_TBL$kana,from="UTF-8",to="SHIFT_JIS")
     H2K_TBL$Hira <- iconv(H2K_TBL$Hira,from="UTF-8",to="SHIFT_JIS")
