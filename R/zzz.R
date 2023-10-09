41f5e2b4315ac6ecd23c5df2de62269a1941e280
Akinori Ito <a_ito@nifty.com>
2023-10-09 19:26:51 +0900

    Removed the code in .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,10 @@
 .onLoad <- function(libname,pkgname) {
-  if (.Platform$OS.type == "windows") {
-    K2P_TBL$kana <- iconv(K2P_TBL$kana,from="UTF-8",to="SHIFT_JIS")
-    H2K_TBL$Hira <- iconv(H2K_TBL$Hira,from="UTF-8",to="SHIFT_JIS")
-    H2K_TBL$Kata <- iconv(H2K_TBL$Kata,from="UTF-8",to="SHIFT_JIS")
-  }
+  # if (.Platform$OS.type == "windows") {
+  #   load("data/K2P_TBL.rda")
+  #   load("data/H2K_TBL.rda")
+  #   K2P_TBL$kana <- iconv(K2P_TBL$kana,from="UTF-8",to="SHIFT_JIS")
+  #   H2K_TBL$Hira <- iconv(H2K_TBL$Hira,from="UTF-8",to="SHIFT_JIS")
+  #   H2K_TBL$Kata <- iconv(H2K_TBL$Kata,from="UTF-8",to="SHIFT_JIS")
+  # }
 }
 
